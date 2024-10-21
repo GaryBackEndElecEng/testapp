d12d34d508691c40a6fc623cdf031218e48ef0e9
gary wallace <garyatgreatvacation4u@gmail.com>
2024-10-21 13:42:24 -0400

    test again-added env(s)

--- src/app/nexPage/nextPage.ts
+++ src/app/nexPage/nextPage.ts
@@ -11,7 +11,7 @@ class NextPage{
         container.style.cssText="margin-inline:auto;margin-block:6rem;display:flex;justify-content:center;align-items:center; gap:2rem";
         const para=document.createElement("p");
         para.style.cssText="margin-inline:auto;padding-inline:1rem;";
-        para.textContent="This is a paragragh here to see if this is shown, given a single page load. If this fails then we need a back end.";
+        para.textContent="This is a paragragh here to see if this is shown, given a single page load. If this fails then we need a back end. test more";
         container.appendChild(para);
         parent.appendChild(container);
     }

1332ed5cd49287576796bf025762ddef65387c46
gary wallace <garyatgreatvacation4u@gmail.com>
2024-10-24 09:49:40 -0400

    removed env in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -141,35 +141,6 @@ const nextConfig = {
       "newmasterconnect.herokuapp.com",
     ],
   },
-  env: {
-    NEXTAUTH_URL:
-      process.env.NODE_ENV === "production"
-        ? process.env.NEXTAUTH_URL
-        : "http://localhost:3000",
-    DATABASE_URL_AWS:
-      process.env.NODE_ENV === "production"
-        ? process.env.DATABASE_URL_AWS
-        : process.env.DATABASEURL,
-    NEXTAUTH_CSRF: process.env.NEXTAUTH_CSRF,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    DATABASEURL: process.env.DATABASEURL,
-    GOOGLE_client_secret: process.env.GOOGLE_client_secret,
-    GOOGLE_client_ID: process.env.GOOGLE_client_ID,
-    EMAIL2: process.env.EMAIL2,
-    EMAIL: process.env.EMAIL,
-    EMAIL_PASS: process.env.EMAIL_PASS,
-    EMAIL_PASS_from_masterconnect919:
-      process.env.EMAIL_PASS_from_masterconnect919,
-    adminpassword: process.env.adminpassword,
-    NEXT_PUBLIC_serverApi: process.env.NEXT_PUBLIC_serverApi,
-    CSRF_SECRET: process.env.CSRF_SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXT_PUBLIC_baseUrl: process.env.NEXT_PUBLIC_baseurl,
-    BUCKET_NAME: process.env.BUCKET_NAME,
-    BUCKET_REGION: process.env.BUCKET_REGION,
-    SDK_ACCESS_KEY: process.env.SDK_ACCESS_KEY,
-    SDK_ACCESS_SECRET: process.env.SDK_ACCESS_SECRET,
-  },
 };
 
 export default nextConfig;

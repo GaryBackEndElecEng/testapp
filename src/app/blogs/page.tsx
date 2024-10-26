c07c4dd34072385e73ff9407d31c3144d78d67da
gary wallace <garyatgreatvacation4u@gmail.com>
2024-10-26 14:39:53 -0400

    removed Meta from blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import type { Metadata, ResolvingMetadata } from "next";
+// import type { Metadata, ResolvingMetadata } from "next";
 import Index from "@/components/blogs/Index";
-import Meta from "@/components/meta/meta";
-export const metadata: Metadata = Meta.metaBlogs();
+// import Meta from "@/components/meta/meta";
+// export const metadata: Metadata = Meta.metaBlogs();
 
 type Props = {
     params: { id: string }

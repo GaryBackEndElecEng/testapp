026614ab9313a4dba1bb83b8b94cb7a06d25796a
gary wallace <garyatgreatvacation4u@gmail.com>
2024-10-27 18:40:44 -0400

    testing meta Chart: layout + non class meta

--- src/app/chart/layout.tsx
+++ src/app/chart/layout.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import Meta from "@/components/meta/meta";
-const newChart = new Meta();
-export const metadata: Metadata = newChart.metaChart();
+// import Meta from "@/components/meta/meta";
+
+export const metadata: Metadata = {
+    title: {
+        default: "chart",
+        template: `%s | chart`,
+
+    },
+    description: "create a Chart",
+    keywords: ["custom charts", "realtime climate change", "make your own graph", "helping you connect", "free Graph making"],
+
+    alternates: {
+        canonical: "/chart",
+        languages: {
+            "en-US": "/en-US",
+            "fr-CA": "/fr-CA"
+        }
+    },
+    openGraph: {
+        title: "Charts",
+        description: 'Customize your chart, free for all',
+        url: "/chart",
+        images: [
+            {
+                url: "/images/gb_logo.png",
+                width: 300,
+                height: 300
+            },
+            {
+                url: "/images/display/symetric.png",
+                width: 400,
+                height: 300
+            },
+            {
+                url: "https://new-master.s3.ca-central-1.amazonaws.com/static/masterultils/logoLarge.png",
+                width: 600,
+                height: 900
+            },
+        ],
+    }
+}
 export default function chartlayout({
     children,
 }: {
